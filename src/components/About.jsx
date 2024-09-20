f85ee84d5d5e7a59eca1ea90da4f955c13a6f611
Gambizzz <anelisegambini@yahoo.fr>
2024-09-20 16:08:44 +0200

    config OK + import logo-name

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/components/About.module.scss';
+
+const About = () => {
+  return (
+    <section id="about" className={styles.about}>
+      <h2>About Me</h2>
+      <p>Here's a little about me...</p>
+    </section>
+  );
+};
+
+export default About;
