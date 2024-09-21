c7fab07bf5af130718683096645420a730e0e4aa
Gambizzz <anelisegambini@yahoo.fr>
2024-09-21 21:15:36 +0200

    modifs cursor + style navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
       <ul>
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
+        <li className={styles.logo}>
+          <img src="/images/logo-name.png" alt="Name logo" className={styles.icon} />
+        </li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
