6f4c1f38ab0c4f18755acdb5f3baeaf4ca31dae9
Gambizzz <anelisegambini@yahoo.fr>
2024-09-21 13:06:15 +0200

    Cursor OK

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
 import '../styles/globals.scss';
+import CustomCursor from '../components/CustomCursor';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <CustomCursor />
+    </>
+  );
 }
 
 export default MyApp;
