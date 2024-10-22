0b478d47104fd67b82ed097e4b10a0fdc883e38d
Gambizzz <anelisegambini@yahoo.fr>
2024-10-22 12:22:36 +0200

    scroll projetcs OK

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,19 +13,104 @@ const Projects = () => {
 
   const projects = {
     'front-end': [
-      { id: 1, name: 'Omiage', image: '/images/projects/omiage.png', description: 'project_omiage', additionalImages: ['/images/projects/omiage1.png', '/images/projects/omiage2.png', '/images/projects/omiage3.png', '/images/projects/omiage4.png', '/images/projects/omiage5.png', '/images/projects/omiage6.png'], mockupImages: ['/images/projects/mu-omiage1.png', '/images/projects/mu-omiage2.png', '/images/projects/mu-omiage3.png'], video: '/videos/omiage.mp4' },
-      { id: 2, name: 'Ubstar', image: '/images/projects/ubstar.png', description: 'project_ubstar', additionalImages: ['/images/projects/ubstar1.png', '/images/projects/ubstar2.png', '/images/projects/ubstar3.png', '/images/projects/ubstar4.png'], mockupImages: ['/images/projects/mu-ubstar1.png', '/images/projects/mu-ubstar2.png', '/images/projects/mu-ubstar3.png'], video: '/videos/ubstar.mp4' },
-      { id: 3, name: 'Landing Page - Portfolio', image: '/images/projects/portfolio.png', description: 'project_portfolio', additionalImages: ['/images/projects/portfolio1.png', '/images/projects/portfolio2.png', '/images/projects/portfolio3.png', '/images/projects/portfolio4.png'], video: '/videos/portfolio.mp4' },
-      { id: 4, name: 'The Surfing Project', image: '/images/projects/surfing.png', description: 'project_surfing', additionalImages: ['/images/projects/surfing1.png', '/images/projects/surfing2.png', '/images/projects/surfing3.png', '/images/projects/surfing4.png'], mockupImages: ['/images/projects/mu-surfing1.png', '/images/projects/mu-surfing2.png', '/images/projects/mu-surfing3.png'], video: '/videos/surfing.mp4' },
+      {
+        id: 1,
+        name: 'Omiage',
+        image: '/images/projects/omiage.png',
+        description: 'project_omiage',
+        additionalImages: ['/images/projects/omiage1.png', '/images/projects/omiage2.png', '/images/projects/omiage3.png', '/images/projects/omiage4.png', '/images/projects/omiage5.png', '/images/projects/omiage6.png'],
+        mockupImages: ['/images/projects/mu-omiage1.png', '/images/projects/mu-omiage2.png', '/images/projects/mu-omiage3.png'],
+        video: '/videos/omiage.mp4',
+        githubLink: 'https://github.com/stevenratton/stevenratton.github.io.git',
+        figmaLink: 'https://www.figma.com/design/ajMMLux9thc4kSp1dmNqmH/omiage?node-id=0-1&t=vmu2J2BskmXUCoVF-1',
+      },
+      {
+        id: 2,
+        name: 'Ubstar',
+        image: '/images/projects/ubstar.png',
+        description: 'project_ubstar',
+        additionalImages: ['/images/projects/ubstar1.png', '/images/projects/ubstar2.png', '/images/projects/ubstar3.png', '/images/projects/ubstar4.png'],
+        mockupImages: ['/images/projects/mu-ubstar1.png', '/images/projects/mu-ubstar2.png', '/images/projects/mu-ubstar3.png'],
+        video: '/videos/ubstar.mp4',
+        githubLink: 'https://github.com/Gambizzz/ubstar_landing_page.git',
+        figmaLink: null,
+      },
+      {
+        id: 3,
+        name: 'Landing Page - Portfolio',
+        image: '/images/projects/portfolio.png',
+        description: 'project_portfolio',
+        additionalImages: ['/images/projects/portfolio1.png', '/images/projects/portfolio2.png', '/images/projects/portfolio3.png', '/images/projects/portfolio4.png'],
+        video: '/videos/portfolio.mp4',
+        githubLink: 'https://github.com/Gambizzz/portfolio_React_Strapi.git',
+        figmaLink: null,
+      },
+      {
+        id: 4,
+        name: 'The Surfing Project',
+        image: '/images/projects/surfing.png',
+        description: 'project_surfing',
+        additionalImages: ['/images/projects/surfing1.png', '/images/projects/surfing2.png', '/images/projects/surfing3.png', '/images/projects/surfing4.png'],
+        mockupImages: ['/images/projects/mu-surfing1.png', '/images/projects/mu-surfing2.png', '/images/projects/mu-surfing3.png'],
+        video: '/videos/surfing.mp4',
+        githubLink: '',
+        figmaLink: '',
+      },
     ],
     'fullstack': [
-      { id: 5, name: 'Menu', image: '/images/projects/menu.png', description: 'project_menu', additionalImages: ['/images/projects/menu1.png', '/images/projects/menu2.png', '/images/projects/menu3.png', '/images/projects/menu4.png', '/images/projects/menu5.png'], mockupImages: ['/images/projects/mu-menu1.png', '/images/projects/mu-menu2.png', '/images/projects/mu-menu3.png'], video: ['/videos/menu.mp4', '/videos/menu1.mp4'] },
-      { id: 6, name: 'Chapatte', image: '/images/projects/chapatte.png', description: 'project_chapatte', additionalImages: ['/images/projects/chapatte1.png', '/images/projects/chapatte2.png', '/images/projects/chapatte3.png', '/images/projects/chapatte4.png'], mockupImages: ['/images/projects/mu-chat1.png', '/images/projects/mu-chat2.png', '/images/projects/mu-chat3.png'], video: '/videos/chapatte.mp4' },
-      { id: 7, name: 'Drumkit', image: '/images/projects/drumkit.png', description: 'project_drumkit', additionalImages: ['/images/projects/drumkit1.png'], mockupImages: ['/images/projects/mu-drum1.png', '/images/projects/mu-drum2.png', '/images/projects/mu-drum3.png'], video: '/videos/drumkit.mp4' },
-      { id: 8, name: 'Film List', image: '/images/projects/filmlist.png', description: 'project_filmlist', additionalImages: ['/images/projects/filmlist1.png', '/images/projects/filmlist2.png', '/images/projects/filmlist3.png'], mockupImages: ['/images/projects/mu-film1.png', '/images/projects/mu-film2.png', '/images/projects/mu-film3.png'], video: '/videos/filmlist.mp4' },
+      { 
+        id: 5,
+        name: 'Menu',
+        image: '/images/projects/menu.png',
+        description: 'project_menu',
+        additionalImages: ['/images/projects/menu1.png', '/images/projects/menu2.png', '/images/projects/menu3.png', '/images/projects/menu4.png', '/images/projects/menu5.png'],
+        mockupImages: ['/images/projects/mu-menu1.png', '/images/projects/mu-menu2.png', '/images/projects/mu-menu3.png'],
+        video: ['/videos/menu.mp4', '/videos/menu1.mp4'],
+        githubLink: ['https://github.com/Gambizzz/back_menu.git', 'https://github.com/Gambizzz/front_menu.git'],
+        figmaLink: 'https://www.figma.com/design/X263wufvh7oE4MfqBedLyD/MENU?node-id=0-1&t=P2FZ1dJPU9TGyTKx-1',
+      },
+      {
+        id: 6,
+        name: 'Chapatte',
+        image: '/images/projects/chapatte.png',
+        description: 'project_chapatte',
+        additionalImages: ['/images/projects/chapatte1.png', '/images/projects/chapatte2.png', '/images/projects/chapatte3.png', '/images/projects/chapatte4.png'],
+        mockupImages: ['/images/projects/mu-chat1.png', '/images/projects/mu-chat2.png', '/images/projects/mu-chat3.png'],
+        video: '/videos/chapatte.mp4',
+        githubLink: 'https://github.com/Gambizzz/chapatte_project.git',
+        figmaLink: 'https://www.figma.com/design/7T7TX2OVGblRWnOhNDL6LY/CHAPATTE?node-id=1-2&t=6C0lQ09Unl1U0C7t-1',
+      },
+      {
+        id: 7,
+        name: 'Drumkit',
+        image: '/images/projects/drumkit.png',
+        description: 'project_drumkit',
+        additionalImages: ['/images/projects/drumkit1.png'],
+        mockupImages: ['/images/projects/mu-drum1.png', '/images/projects/mu-drum2.png', '/images/projects/mu-drum3.png'],
+        video: '/videos/drumkit.mp4',
+        githubLink: '',
+        figmaLink: '',
+      },
+      {
+        id: 8,
+        name: 'Film List',
+        image: '/images/projects/filmlist.png',
+        description: 'project_filmlist',
+        additionalImages: ['/images/projects/filmlist1.png', '/images/projects/filmlist2.png', '/images/projects/filmlist3.png'],
+        mockupImages: ['/images/projects/mu-film1.png', '/images/projects/mu-film2.png', '/images/projects/mu-film3.png'],
+        video: '/videos/filmlist.mp4',
+        githubLink: 'https://github.com/Gambizzz/films_list_JS.git',
+        figmaLink: null,
+      },
     ],
     'design': [
-      { id: 9, name: 'Maxime Vincenti', image: '/images/projects/corde.png', description: 'project_maxime_vincenti', additionalImages: ['/images/projects/recto.png', '/images/projects/verso.png']},
+      {
+        id: 9,
+        name: 'Maxime Vincenti',
+        image: '/images/projects/corde.png',
+        description: 'project_maxime_vincenti',
+        additionalImages: ['/images/projects/recto.png', '/images/projects/verso.png']
+      },
     ],
   };
 
@@ -40,6 +125,8 @@ const Projects = () => {
     setIsPopupOpen(false);
     setSelectedProject(null);
   };
+  
+  const hasMultipleProjects = filteredProjects.length > 1;
 
   return (
     <section id="projects" className={styles.projects}>
@@ -71,7 +158,7 @@ const Projects = () => {
         </div>
 
         <div className={styles.projectsContainer}>
-          <div className={styles.projectsRow}>
+          <div className={`${styles.projectsRow} ${hasMultipleProjects ? styles.animate : ''}`}>
             {filteredProjects.map((project) => (
               <div key={project.id} className={styles.bigCard}>
                 <div
@@ -105,22 +192,41 @@ const Projects = () => {
             <h2>{t('projectDescript')}</h2>
             <p>{t(selectedProject.description)}</p>
             <ul className={styles.socialLinks}>
-              <li
-                onMouseEnter={() => setHoveredLink('github')}
-                onMouseLeave={() => setHoveredLink(null)}
-              >
-                <a href="" target="_blank" rel="noopener noreferrer">
-                  <FaGithub />
-                </a>
-              </li>
-              <li
-                onMouseEnter={() => setHoveredLink('linkedin')}
-                onMouseLeave={() => setHoveredLink(null)}
-              >
-                <a href="" target="_blank" rel="noopener noreferrer">
-                  <FaFigma />
-                </a>
-              </li>
+              {Array.isArray(selectedProject.githubLink) ? (
+                selectedProject.githubLink.map((link, index) => (
+                  <li
+                    key={`github-link-${index}`}
+                    onMouseEnter={() => setHoveredLink('github')}
+                    onMouseLeave={() => setHoveredLink(null)}
+                  >
+                    <a href={link} target="_blank" rel="noopener noreferrer">
+                      <FaGithub />
+                    </a>
+                  </li>
+                ))
+              ) : (
+                selectedProject.githubLink && (
+                  <li
+                    onMouseEnter={() => setHoveredLink('github')}
+                    onMouseLeave={() => setHoveredLink(null)}
+                  >
+                    <a href={selectedProject.githubLink} target="_blank" rel="noopener noreferrer">
+                      <FaGithub />
+                    </a>
+                  </li>
+                )
+              )}
+
+              {selectedProject.figmaLink && (
+                <li
+                  onMouseEnter={() => setHoveredLink('figma')}
+                  onMouseLeave={() => setHoveredLink(null)}
+                >
+                  <a href={selectedProject.figmaLink} target="_blank" rel="noopener noreferrer">
+                    <FaFigma />
+                  </a>
+                </li>
+              )}
             </ul>
 
             <h2>{t('projectVisu')}</h2>
@@ -159,17 +265,14 @@ const Projects = () => {
               )}
             </div>
 
-
             <h2>{t('projectVid')}</h2>
             {selectedProject.video ? (
               typeof selectedProject.video === 'string' ? (
-                // show one video only
                 <video className={styles.videoPlayer} controls>
                   <source src={selectedProject.video} type="video/mp4" />
                   {t('unsupportVid')}
                 </video>
               ) : (
-                // show many videos
                 selectedProject.video.map((videoSrc, index) => (
                   <video key={index} className={styles.videoPlayer} controls>
                     <source src={videoSrc} type="video/mp4" />
@@ -194,10 +297,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-
-
-
-
-
-
